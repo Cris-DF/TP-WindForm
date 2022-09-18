bfc3b58d3c084a250c36ebf88d29db07042b5ba2
Cris-DF <cristianflores6100@gmail.com>
2022-09-18 19:08:50 -0300

    funcion para editar articulo, sin validaciones

--- TPWinForm_Sanchez_Flores/FormEditarArticulo.cs
+++ TPWinForm_Sanchez_Flores/FormEditarArticulo.cs
@@ -111,7 +111,7 @@ namespace TPWinForm_Sanchez_Flores
                 }
                 else
                 {
-                    //modificar
+                    dataArticulo.modificar(articulo);
                 }
 
                 Close();
